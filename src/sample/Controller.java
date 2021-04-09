47a0cd0e08bd69fb42057f163929c5686a5ee56e
DimitrisMitroulis <amfimitrul@gmail.com>
2021-04-09 17:14:59 +0300

    allages giorgou

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -23,7 +23,7 @@ import java.sql.*;
 import java.util.ResourceBundle;
 
 public class Controller {
-
+    public static String rId = "hello";
     @FXML
     Button Login,Sign_up,Go_backBtn;
     @FXML
